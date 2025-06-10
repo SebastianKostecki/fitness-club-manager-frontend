0a1b716054c1994c8e2a2589e2b0ed8f6c1f3e20
SebastianKostecki <sebastiankostecki25@gmail.com>
2025-06-11 01:35:37 +0200

    strona startowa

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
@@ -13,6 +18,10 @@ export class StartComponent {
 
   goToLogin() {
     this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
   }
 
 }
